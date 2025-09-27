bc15ef963af26012866565ee81048fb92fed7c69
egoist1313 <13egoist@gmail.com>
2025-09-27 09:31:09 +0900

    1

--- LuaDunaevskiy/CEF.lua
+++ LuaDunaevskiy/CEF.lua
@@ -1,20 +1,41 @@
-local script_name = 'CEF'
-local script_version = '14/08/2025'
-script_version_number = 1
-script_dependencies = {'sampfuncs'}
-
-local sampfuncs = require 'sampfuncs'
-
-local is_cursor_visible = false -- Переменная для отслеживания состояния курсора
+local script_name = 'UpdateIndexHTML'
+local script_author = 'AlexDunaevskiy'
+local script_version = '1.0.0'
+local ffi = require 'ffi'
+require 'moonloader'
 
 function main()
-    if not isSampfuncsLoaded() or not isSampLoaded() then return end
-    while true do
-        wait(0)
+    local file_path = getGameDirectory() .. "\\SAMP-RP\\CEF\\frontend\\index.html"
+    local file = io.open(file_path, "r")
+    if not file then
+        print("Ошибка: Не удалось открыть index.html")
+        return
+    end
+    
+    local content = file:read("*a")
+    file:close()
+    local custom_script = 'src="./static/js/custom.js"'
+    local custom_css = 'href="./static/css/custom.css"'
+    
+    if not content:find(custom_script, 1, true) or not content:find(custom_css, 1, true) then
+        content = content:gsub('<html lang="en">', '<html lang="ru">')
+        content = content:gsub(
+            '(<script defer="defer" src="./static/js/main%.[^"]+%.js"></script>)',
+            '%1\n    <script defer="defer" src="./static/js/custom.js"></script>'
+        )
+        content = content:gsub(
+            '(<link href="./static/css/main%.[^"]+%.css" rel="stylesheet">)',
+            '%1\n    <link href="./static/css/custom.css" rel="stylesheet">'
+        )
+        local file = io.open(file_path, "w")
+        if file then
+            file:write(content)
+            file:close()
+            sampAddChatMessage("[UpdateIndexHTML] index.html обновлен, перезапустите игру", 0x00FF00)
+        else
+            print("Ошибка: Не удалось записать в index.html")
+        end
+    else
+        print("Строки уже присутствуют в index.html")
     end
 end
-
-sampRegisterChatCommand('cef', function()
-    is_cursor_visible = not is_cursor_visible -- Переключаем состояние
-    showCursor(is_cursor_visible) -- Показываем или скрываем курсор
-end)
